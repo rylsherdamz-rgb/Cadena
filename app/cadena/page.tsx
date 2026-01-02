8303814faea1fcf2c93fc095e153590a76f37875
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-01-02 10:39:31 +0800

    added more basic ui to the cadena page

--- app/cadena/page.tsx
+++ app/cadena/page.tsx
@@ -1,8 +1,16 @@
-
+import CadenaMainContent from "@/components/CadenaMainContent"
 
 
 export default function Cadena() {
-    return <div className="w-full h-screen bg-white">
+    return <div className="w-full px-5 py-6  h-screen bg-white">
+        <div className="text-black flex flex-row gap-x-10 justify-end px-10  ">
+       <button className=" bg-blue-400 px-5 rounded-2xl py-2">Admin DashBoard</button> 
+       <button className=" bg-violet-400 px-5 rounded-2xl py-2">Connect your wallet</button> 
+
+        </div>
+        <div className="w-full py-5">
+            <CadenaMainContent />
+        </div>
 
     </div>
 }
---
+++ components/CadenaMainContent.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default function CadenaMainContent() {
+    return <div className="w-full h-[70vh] bg-gray-200 rounded-2xl">
+
+    </div>
+}
