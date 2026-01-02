23d5ec6c0d10bd15cb29fbb07e34f0602c3992c1
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-01-02 11:02:57 +0800

    added just basic logic

--- components/SearchBarComponent.tsx
+++ components/SearchBarComponent.tsx
@@ -1,7 +1,19 @@
+import React, { useState } from "react"
+
 
 
 export default function SearchBarComponent() {
+    const [search, setSearch] = useState<string>("")
+    
+    const handleSubmitSearch = (e :  React.ChangeEvent<HTMLInputElement>) => {
+     setTimeout(() => setSearch(e.target.value), 500)
+    }
     return <div className="w-full h-full">
-        <input className="focus:outline-none bg-white px-4 py-2 rounded-2xl text-black"  placeholder="Search Contracts.." />
+        <input
+        value={search}
+        onChange={(e) =>{
+            setTimeout(() => setSearch(e.target.value), 500)
+        }}
+         className="focus:outline-none bg-white px-4 py-2 rounded-2xl text-black"  placeholder="Search Contracts.." />
     </div>
 }
